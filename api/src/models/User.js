154b1694c7a73aec62a6d0f16533425352a15684
horo98 <martinhorodeski@gmail.com>
2022-12-10 21:28:34 -0300

    first commit

---
+++ api/src/models/User.js
@@ -0,0 +1,84 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "user",
+    {
+      id: {
+        type: DataTypes.NUMERIC,
+        allowNull: false,
+        primaryKey: true,
+      },
+      username: {
+        type: DataTypes.CHAR,
+        allowNull: false,
+        unique: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          len: {
+            args: [2, 50],
+            msg: "The name must only contain at least two letters",
+          },
+        },
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        validate: {
+          len: {
+            args: [4, 255],
+            msg: "The password length should be between 6 and 255 characters.",
+          },
+        },
+      },
+      identification: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          is: {
+            args: /^((\+|)[0-9]{1,3}(-|\s)[0-9]{2,4}(-|\s)[0-9]{6,8})$/,
+            msg: "Enter a valid phone",
+          },
+        },
+      },
+      isAdmin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      address: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      isActive: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+    },
+    {
+      timestamp: false,
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
+};
